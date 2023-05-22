d5fde949f8d11460df9ff6a54fa1766dc346b428
orlandoduranpy <orlandoduranpy@outlook.es>
2023-05-21 18:20:02 -0600

    Navbar listo

--- expediente/static/js/app.js
+++ expediente/static/js/app.js
@@ -1,1 +1,13 @@
-console.log('JS cargado correctamente');
+document.addEventListener('DOMContentLoaded', function () {
+  /* ========================================
+    Abrir y cerrar el menu de sesion
+  ========================================= */
+  let showToggle = document.getElementById('showToggle');
+  let subMenu = document.getElementById('subMenu');
+  showToggle.addEventListener('click', function () {
+    subMenu.classList.toggle('open-menu');
+  });
+
+
+  
+});
